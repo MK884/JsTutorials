7fbc666d31911e6aaf302996525a766c2a19b945
MK884 <marchantkhalid7810@gmail.com>
2023-11-14 21:18:26 +0530

    Bulb Project

--- on_off_Bulb/script.js
+++ on_off_Bulb/script.js
@@ -1,16 +1,18 @@
 var Img = document.querySelector('img');
 var onBtn = document.getElementById('bulb-on');
 var offBtn = document.getElementById('bulb-off');
+var body = document.querySelector('body');
 
 onBtn.addEventListener('click', function(){
     
     Img.src = 'https://i.postimg.cc/6QyTynzr/bulb-on.png';
-    
+    body.style.backgroundColor = "rgb(224, 199, 52)"
 })
 
 offBtn.addEventListener('click', function(){
     
     Img.src = 'https://i.postimg.cc/KjK1wL3c/bulb-off.png';
+    body.style.backgroundColor = "#111"
 
     
 })
