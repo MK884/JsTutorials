42ddfe9bce4ff4902037e15a259005d4d6d12b77
MK884 <marchantkhalid7810@gmail.com>
2023-11-17 21:39:11 +0530

    Country Drop-down

--- drop-down/script.js
+++ drop-down/script.js
@@ -2,7 +2,9 @@ var iconCart = document.querySelector('i');
 var select = document.querySelector('.select');
 var selected = document.querySelector('.selected');
 var menu = document.querySelector('.menu');
-var menuItem = document.querySelectorAll('.menu li');
+
+const allCountry = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+
 
 const toggleMenu =()=>{
     select.classList.toggle('select-open');
@@ -11,11 +13,21 @@ const toggleMenu =()=>{
 select.addEventListener('click',function(){
     toggleMenu();
 })
+let li='';
+allCountry.forEach(function(country){
+    li +=`<li>${country}</li>`;
+})
+menu.innerHTML = li;
+var menuItem = document.querySelectorAll('.menu li');
+
 
 menuItem.forEach(function(Item){
     Item.addEventListener('click',function(){
-        selected.innerHTML = Item.innerHTML;
-        document.querySelector('.menu .active').classList.remove('active');
+        selected.innerText = Item.innerText;
+        var active =  document.querySelector('.menu .active')
+        if(active !=null){
+            active.classList.remove('active');
+        }
         Item.classList.add('active');
         toggleMenu()
     })
